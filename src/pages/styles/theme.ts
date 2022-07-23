c401dfa69785300c1b1fc057142666db6ad330f8
niubajr19 <lucas.antonio199@outlook.com>
2022-07-23 20:59:43 -0300

    Styled - Adicionando tipagem do styled

---
+++ src/pages/styles/theme.ts
@@ -0,0 +1,11 @@
+const theme = {
+    color: {
+        background: '#f9f6fc',
+        text: '#424242',
+
+        primary: '#5d00bf',
+        disabled: '#e0e0e0',
+    },
+}
+
+export default theme
